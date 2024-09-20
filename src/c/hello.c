39390261776d9e3f5fcff0ab94df209aa2678550
Cookie <171355049+cookieShepsky@users.noreply.github.com>
2024-09-20 19:02:26 +0200

    chore: add stuff

--- src/c/hello.c
+++ src/c/hello.c
@@ -2,7 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int myReturn(int number) {
+  /*number++;*/
+  return number++;
+}
+
 int main(){
-  printf("Hello, World!");
+  //printf("Hello, World!");
+  int number = 9;
+  printf("%d", myReturn(number));
+
   return 0;
 }
